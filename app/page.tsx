e2a0ebba929a658f06c8a6ae19cbf8eb62bbc342
tuansydau <tukauu@gmail.com>
2024-06-29 17:42:35 -0400

    add: style changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,48 @@
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen w-2/3 py-4 px-2 space-y-4"> 
-    <h1 className="w-full text-3xl font-semibold">
-      This is Tuan&apos;s website right now
-    </h1>
-    <p className="text-lg">
-      <b>Apr 20</b>
-      <br />
-      <br />
-      And for now, I think that&apos;s all that I am going to do. I&apos;ve got so many projects in the pipeline that I want to work on and deploy, and I&apos;ll list them all here as they come!
-      <br />
-      <br /> 
-      This current website is being hosted on my $6/mo digital ocean droplet with 1vcpu and 25gb of storage, and I cannot explain the amount of joy that hosting on baremetal brings to me.
-      <br />
-      <br />
-      One day when I sort out my spending habits (<b>FUCK YOU TEENAGE ENGINEERING and TORONTO RENT</b>), I will finally get an extra cpu on this droplet, and I will go to town on this site. For now, my tentative website is at <a href="http://tuan-dau.vercel.app" target="_blank" className="text-blue-500 underline">tuan-dau.vercel.app</a>, whenever I choose to work on it.
-      <br />
-      <br />  
-      Every work demo, every side project, every file that I intend to host - it&apos;s all going to be hosted here, on my incredible little droplet.
-      <br />
-      <br />
-      Until something of interest comes to this webpage, feel free to check out my current workplace, <a href="https://www.arimadata.com" className="text-blue-500 underline" target="_blank">Arima Data</a>. 
-      <br />
-      <br />
-      <span className="text-blue-400">B</span>
-      <span className="text-indigo-300">i</span>
-      <span className="text-red-500">g </span>
-      <span className="text-yellow-500">t</span>
-      <span className="text-green-500">h</span>
-      <span className="text-red-300">i</span>
-      <span className="text-cyan-500">n</span>
-      <span className="text-pink-500">g</span>
-      <span className="text-pink-900">s </span>
-      <span className="text-teal-500">c</span>
-      <span className="text-red-100">o</span>
-      <span className="text-lime-500">m</span>
-      <span className="text-orange-500">i</span>
-      <span className="text-amber-500">n</span>
-      <span className="text-red-700">g</span>
-      <span className="text-blue-500">.</span>
-
-
-     
-    </p>
-    
+    <div className="flex flex-col min-h-screen w-full lg:items-center py-4 px-2 space-y-4">
+      <div className="w-full lg:w-1/2">
+        <h1 className="text-3xl font-semibold">
+          This is Tuan&apos;s website right now
+        </h1>
+        <p className="text-lg">
+          <b>Apr 20</b>
+          <br />
+          <br />
+          And for now, I think that&apos;s all that I am going to do. I&apos;ve got so many projects in the pipeline that I want to work on and deploy, and I&apos;ll list them all here as they come!
+          <br />
+          <br /> 
+          This current website is being hosted on my $6/mo digital ocean droplet with 1vcpu and 25gb of storage, and I cannot explain the amount of joy that hosting on baremetal brings to me.
+          <br />
+          <br />
+          One day when I sort out my spending habits (<b>FUCK YOU TEENAGE ENGINEERING and TORONTO RENT</b>), I will finally get an extra cpu on this droplet, and I will go to town on this site. For now, my tentative website is at <a href="http://tuan-dau.vercel.app" target="_blank" className="text-blue-500 underline">tuan-dau.vercel.app</a>, whenever I choose to work on it.
+          <br />
+          <br />  
+          Every work demo, every side project, every file that I intend to host - it&apos;s all going to be hosted here, on my incredible little droplet.
+          <br />
+          <br />
+          Until something of interest comes to this webpage, feel free to check out my current workplace, <a href="https://www.arimadata.com" className="text-blue-500 underline" target="_blank">Arima Data</a>. 
+          <br />
+          <br />
+          Thanks,
+          <br />
+          <span className="text-blue-400">B</span>
+          <span className="text-indigo-300">i</span>
+          <span className="text-red-500">g </span>
+          <span className="text-yellow-500">t</span>
+          <span className="text-green-500">h</span>
+          <span className="text-red-300">i</span>
+          <span className="text-cyan-500">n</span>
+          <span className="text-pink-500">g</span>
+          <span className="text-pink-900">s </span>
+          <span className="text-teal-500">c</span>
+          <span className="text-red-100">o</span>
+          <span className="text-lime-500">m</span>
+          <span className="text-orange-500">i</span>
+          <span className="text-amber-500">n</span>
+          <span className="text-red-700">g</span>
+        </p>
+      </div>
     </div>
   );
 }
