aa27d9dde1dc140f11abbad886658a566c1674d9
tuansydau <tukauu@gmail.com>
2024-12-09 08:25:46 -0500

    add: new fonts and style cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,90 @@
+import "@/public/styles/cabinet-grotesk.css";
+
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen w-full lg:items-center py-4 px-2 space-y-4">
-      <div className="w-full lg:w-1/2">
-        <h1 className="text-3xl font-semibold">
-          This is Tuan&apos;s website right now
-        </h1>
-        <p className="text-lg">
-          <b>Apr 20</b>
-          <br />
-          <br />
-          And for now, I think that&apos;s all that I am going to do. I&apos;ve got so many projects in the pipeline that I want to work on and deploy, and I&apos;ll list them all here as they come!
-          <br />
-          <br /> 
-          This current website is being hosted on my $6/mo digital ocean droplet with 1vcpu and 25gb of storage, and I cannot explain the amount of joy that hosting on baremetal brings to me.
-          <br />
-          <br />
-          One day when I sort out my spending habits (<b>FUCK YOU TEENAGE ENGINEERING and TORONTO RENT</b>), I will finally get an extra cpu on this droplet, and I will go to town on this site. For now, my tentative website is at <a href="http://tuan-dau.vercel.app" target="_blank" className="text-blue-500 underline">tuan-dau.vercel.app</a>, whenever I choose to work on it.
-          <br />
-          <br />  
-          Every work demo, every side project, every file that I intend to host - it&apos;s all going to be hosted here, on my incredible little droplet.
-          <br />
-          <br />
-          Until something of interest comes to this webpage, feel free to check out my current workplace, <a href="https://www.arimadata.com" className="text-blue-500 underline" target="_blank">Arima Data</a>. 
-          <br />
-          <br />
-          Thanks,
-          <br />
-          <span className="text-blue-400">B</span>
-          <span className="text-indigo-300">i</span>
-          <span className="text-red-500">g </span>
-          <span className="text-yellow-500">t</span>
-          <span className="text-green-500">h</span>
-          <span className="text-red-300">i</span>
-          <span className="text-cyan-500">n</span>
-          <span className="text-pink-500">g</span>
-          <span className="text-pink-900">s </span>
-          <span className="text-teal-500">c</span>
-          <span className="text-red-100">o</span>
-          <span className="text-lime-500">m</span>
-          <span className="text-orange-500">i</span>
-          <span className="text-amber-500">n</span>
-          <span className="text-red-700">g</span>
-        </p>
+    <>
+      <div
+        style={{ fontFamily: "CabinetGrotesk-Variable" }}
+        className="flex flex-col min-h-screen w-full lg:items-center px-2 space-y-4 py-4"
+      >
+        <h1 className="text-3xl font-bold">Playground here</h1>
+
+        <div className="flex">
+          {/* bottom proj card */}
+          <div className="w-64 h-72 border border-black bg-gradient-to-t from-lime-300 to-lime-500 to-85%">
+            {/* top proj card */}
+            <div className="w-64 h-72 ml-4 mt-4 hover:mt-0 hover:ml-0 hover:border-l-0 hover:border-t-0 border border-black bg-white transition-all duration-500 ease-in-out">
+              <div className="pl-2 flex w-full h-8 border-b border-black items-center text-xs">
+                Feb 2024 - Nov 2024
+              </div>
+              <div className="pl-2 flex w-full h-16 border-b border-black items-center text-[32px] font-bold">
+                Arima Data
+              </div>
+              <div className="pl-2 flex w-full h-32 border-b border-black items-center bg-[rgb(190,217,115)]" />
+              <div className="pl-2 flex w-full h-16 border-black items-center text-sm">
+                Home Page Redesign / Authentication Routes
+              </div>
+            </div>
+          </div>
+
+          <div className="w-12"></div>
+          {/* bottom proj card */}
+          <div className="w-64 h-72 border border-black bg-gradient-to-t from-red-100  to-red-500">
+            {/* top proj card */}
+            <div className="w-64 h-72 ml-4 mt-4 hover:mt-0 hover:ml-0 hover:border-l-0 hover:border-t-0 border border-black bg-white transition-all duration-500 ease-in-out">
+              <div className="pl-2 flex w-full h-8 border-b border-black items-center text-xs">
+                Winter 2022
+              </div>
+              <div className="pl-2 flex w-full h-16 border-b border-black items-center text-[32px] font-bold">
+                Tesla
+              </div>
+              <div className="pl-2 flex w-full h-32 border-b border-black items-center bg-red-500" />
+              <div className="pl-2 flex w-full h-16 border-black items-center text-sm">
+                Infotainment / Navigation Systems Team
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-12" />
+
+        <div className="flex">
+          {/* bottom proj card */}
+          <div className="w-64 h-72 border border-black bg-gradient-to-t from-[#a6a6a6]  to-[#000000]">
+            {/* top proj card */}
+            <div className="w-64 h-72 ml-4 mt-4 hover:mt-0 hover:ml-0 hover:border-l-0 hover:border-t-0 border border-black bg-white transition-all duration-500 ease-in-out">
+              <div className="pl-2 flex w-full h-8 border-b border-black items-center text-xs">
+                Fall 2021
+              </div>
+              <div className="pl-2 flex w-full h-16 border-b border-black items-center text-[32px] font-bold">
+                CertiK
+              </div>
+              <div className="pl-2 flex w-full h-32 border-b border-black items-center bg-[#6c6c6c]" />
+              <div className="pl-2 flex w-full h-16 border-black items-center text-sm">
+                Security Tools / Static Analysis Team
+              </div>
+            </div>
+          </div>
+
+          <div className="w-12"></div>
+          {/* bottom proj card */}
+          <div className="w-64 h-72 border border-black bg-gradient-to-t from-blue-300  to-blue-700">
+            {/* top proj card */}
+            <div className="w-64 h-72 ml-4 mt-4 hover:mt-0 hover:ml-0 hover:border-l-0 hover:border-t-0 border border-black bg-white transition-all duration-500 ease-in-out">
+              <div className="pl-2 flex w-full h-8 border-b border-black items-center text-xs">
+                Fall 2021
+              </div>
+              <div className="pl-2 flex w-full h-16 border-b border-black items-center text-[32px] font-bold">
+                4PAY
+              </div>
+              <div className="pl-2 flex w-full h-32 border-b border-black items-center bg-blue-900" />
+              <div className="pl-2 flex w-full h-16 border-black items-center text-sm">
+                Money Transfers / Fraud Detection
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
